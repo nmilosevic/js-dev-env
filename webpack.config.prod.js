7f479a886538baa84f26c78ae3c6d08fb7fe0851
Nenad Milosevic <milos3vicn@gmail.com>
2018-05-16 08:58:20 +0100

    Added scripts for running the production environment and useMockApi switch

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,21 +1,34 @@
 import path from 'path';
+import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     entry: [
         path.resolve(__dirname, 'src/index')
     ],
     watch: true,
     target: 'web',
     output: {
-        path: path.resolve(__dirname, 'src'),
+        path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
         filename: 'bundle.js'
     },
     plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
         new HtmlWebpackPlugin({
             template: 'src/index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes:true,
+                useShortDoctype:true,
+                removeEmptyAttributes:true,
+                keepClosingSlash:true,
+                minifyJS:true,
+                minifyCSS:true,
+                minifyURLs:true
+            },
             inject: true
         })
     ],
